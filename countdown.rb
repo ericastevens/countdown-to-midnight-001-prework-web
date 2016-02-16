57cf9442e5b2cd69f17ab1721201fc30446d9eb0
ericastevens <estevens@nyc.yearup.org>
2016-02-16 05:46:15 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(num)
+
+  while num > 0 do
+    puts "#{num} SECOND(S)!"
+    num -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+countdown(10)
+
+def countdown_with_sleep(num)
+  
+  while num > 0 do
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
 end
